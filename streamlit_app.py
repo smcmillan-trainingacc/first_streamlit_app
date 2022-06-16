5573a0f2b1af7f59f591554749bc11437e2bde16
smcmillan-trainingacc <107615920+smcmillan-trainingacc@users.noreply.github.com>
2022-06-16 10:30:22 +0100

    Update streamlit_app.py
    changing csv index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# changing index to the fruit rather than the number
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # adding a list so people can pick the fruit they want
