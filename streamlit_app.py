45cabfd53bc3cf34c4971d60f68a85a4eef88970
smcmillan-trainingacc <107615920+smcmillan-trainingacc@users.noreply.github.com>
2022-06-16 14:23:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,6 @@ streamlit.header('Fruityvice Fruit Advice')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalised)
