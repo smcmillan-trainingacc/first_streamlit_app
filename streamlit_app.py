f9a815a0405123969e286fe76bbbc53a876ae161
smcmillan-trainingacc <107615920+smcmillan-trainingacc@users.noreply.github.com>
2022-06-16 10:29:12 +0100

    Update streamlit_app.py
    forgot a comma

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 
 # adding a list so people can pick the fruit they want
-streamlit.multiselect("Pick some fruits: " list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 
 # display the table on the page
 streamlit.dataframe(my_fruit_list)
